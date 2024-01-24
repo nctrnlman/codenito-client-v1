76cc9f5c4f38ab9e8c14dc11c96e3fccf70fecc9
Rhazes Devino <rhazesd@gmail.com>
2024-01-24 18:51:12 +0800

    check

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "192.168.0.249",
-    port: 8000,
-  },
+  // server: {
+  //   host: "192.168.1.12",
+  //   port: 8000,
+  // },
   plugins: [react()],
 });
